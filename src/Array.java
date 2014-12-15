0609411e61cb721c41f4d662c6e0ac39e25da997
Sonia Ortiz de Arri <soniaodea@gmail.com>
2014-12-15 14:25:00 +0100

    l

---
+++ src/Array.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class Array {
+	public static void main(String[] args) {
+			//se crea el array
+		 //	int [] numeros;//definicion 
+		 //	numeros = new int [5];//indicar espacio 
+		 int numeros []= { 2, 3, 5, 7, 8};//declarar valores
+
+		 for (int n =0; n<numeros.length; n++){
+		 	System.out.println(numeros[n]);
+		 }
+	}
+	
+}
